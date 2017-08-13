11909afe2814c75f236f67b6223b8aebbb563b99
polynomia <wyrsjtu@163.com>
2017-08-13 22:31:34 +0800

    not finished

---
+++ codeforce/814D-discotheque.cpp
@@ -0,0 +1,19 @@
+#include<cstdio>
+#include<vector>
+#include<iostream>
+
+using namespace std;
+int x[1005],y[1005],r[1005];
+
+bool is_contain(int u,int v){
+	
+
+int main(){
+	int n,i;
+	scanf("%d",&n);
+	for(i = 0; i < n; i++){
+		scanf("%d%d%d",x+i,y+i,r+i);
+	}
+	
+	return 0;
+}
